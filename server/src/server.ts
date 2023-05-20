a58ee605aca6adc94926fa50813a928a07577b9d
Arthur <martinsarthur07@gmail.com>
2023-05-20 13:02:52 -0300

    feat: authentication route
    update: add authentication to all memories routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,9 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { authRoutes } from './routes/auth'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
@@ -7,9 +11,17 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: 'hahaha',
+})
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
+
 app
   .listen({
     port: 8081,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('running on port 8081')
